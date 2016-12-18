6c3e30d5502ae895f97d8c0ab4ee312e1d5a8c10
Lucas <lffsantos@gmail.com>
2016-12-18 19:03:12 +0000

    refactor and optimize calls

--- code_two/postcodeuk/__init__.py
+++ code_two/postcodeuk/__init__.py
@@ -27,8 +27,6 @@ class PostCodeUk:
     OUTWARD_PATTERN = '|'.join(list(OUTWARD_PATTERNS.values()))
     VALID_POSTCODE_PATTERN = r"^("+OUTWARD_PATTERN+")[ ]*("+INWARD_PATTERN+"$)"
     POSTCODE_PATTERN_IN_TEXT = r"("+OUTWARD_PATTERN+") ("+INWARD_PATTERN+")"
-    _inward = None
-    _outward = None
 
     def __init__(self, post_code):
         if not isinstance(post_code, str):
@@ -38,13 +36,14 @@ class PostCodeUk:
                 )
             )
         self.post_code = post_code
+        self._is_valid = self._is_valid()
 
     @staticmethod
     def _validate(post_code):
         regex_postcode = re.match(PostCodeUk.VALID_POSTCODE_PATTERN, post_code)
         return regex_postcode if regex_postcode else None
 
-    def is_valid(self):
+    def _is_valid(self):
         if not self.post_code:
             return False
 
@@ -55,30 +54,33 @@ class PostCodeUk:
         self._outward, self._inward = code.groups()
         return True
 
+    def is_valid(self):
+        return self._is_valid
+
     def get_outward(self):
-        return self._outward if self.is_valid() else None
+        return self._outward if self._is_valid else None
 
     def get_inward(self):
-        return self._inward if self.is_valid() else None
+        return self._inward if self._is_valid else None
 
-    @staticmethod
-    def random_postcode():
-        pattern = r"^("+PostCodeUk.OUTWARD_PATTERN+")[ ]("+PostCodeUk.INWARD_PATTERN+"$)"
+    @classmethod
+    def random_postcode(cls):
+        pattern = r"^("+cls.OUTWARD_PATTERN+")[ ]("+cls.INWARD_PATTERN+"$)"
         return xeger(pattern)
 
-    @staticmethod
-    def find_all_in_text(text, post_codes=[]):
+    @classmethod
+    def find_all_in_text(cls, text, post_codes=[]):
         if post_codes and not isinstance(post_codes, list):
             raise TypeError(
                 'postcode should be instance of "list" but received a "{}"'.format(
                     type(post_codes).__name__
                 )
             )
-        pattern = PostCodeUk.POSTCODE_PATTERN_IN_TEXT
+        pattern = cls.POSTCODE_PATTERN_IN_TEXT
 
         if post_codes:
             if not all(
-                    PostCodeUk._validate(code) if isinstance(code, str) else None
+                    cls._validate(code) if isinstance(code, str) else None
                     for code in post_codes):
                 return "all postcodes should be valid"
             pattern = '|'.join(post_codes)
